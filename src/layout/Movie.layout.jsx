a9ae62d46ff220ef6a0111bbb948f29d2163c9e6
Aakash Singh <aakashsingh080598@gmail.com>
2024-03-25 07:16:33 +0000

    basic structure

---
+++ src/layout/Movie.layout.jsx
@@ -0,0 +1,17 @@
+//rafce
+import React from "react";
+import MovieNavbar from "../components/Navbar/MovieNavbar.component";
+
+const MovieLayoutHoc =
+  (Component) =>
+  ({ ...props }) => {
+    return (
+      <div>
+        <MovieNavbar />
+        <Component {...props} />
+        <div>Footer</div>
+      </div>
+    );
+  };
+
+export default MovieLayoutHoc;
